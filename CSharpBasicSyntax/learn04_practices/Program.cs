1d889a7270bf8656336036c5c757040eff3b94bd
prince <2300799210@qq.com>
2021-05-28 23:00:00 +0800

    两个小练习跟类型转换的补充

---
+++ CSharpBasicSyntax/learn04_practices/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace learn04_practices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //计算46天是几周几天
+            //int days = 46;
+            //int week = 46 / 7;
+            //int day = 46 % 7;
+            //Console.WriteLine("{0}天是{1}周{2}天", days, week, day);
+
+            //计算107653秒是几天几小时几分钟几秒
+            int seconds = 107653;
+            int day = seconds / (60 * 60 * 24);
+            int house = seconds % (60 * 60 * 24) / (60 * 60);
+            int mins = seconds % (60 * 60 * 24) % (60 * 60) / 60;
+            int secs = seconds % (60 * 60 * 24) % (60 * 60) % 60;
+            Console.WriteLine("{0}秒是{1}天{2}时{3}分钟{4}秒", seconds, day, house, mins, secs);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ CSharpBasicSyntax/learn05_convert/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace learn05_convert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 类型转换
+             * 当数据类型步兼容时采用 Convert.To数据类型(待转换的值)；
+             * 要求：数据的值形式上能转换得过去
+             */
+
+            Console.WriteLine("请输入您的姓名：");
+            string strName = Console.ReadLine();
+            Console.WriteLine("请输入语文成绩：");
+            string strChinese = Console.ReadLine();
+            Console.WriteLine("请输入数学成绩：");
+            string strMath = Console.ReadLine();
+            Console.WriteLine("请输入英语成绩：");
+            string strEnglish = Console.ReadLine();
+
+            double chinese = Convert.ToDouble(strChinese);
+            double math = Convert.ToDouble(strMath);
+            double english = Convert.ToDouble(strEnglish);
+
+            Console.WriteLine("{0}同学你好，你的总分是{1}分，平局分是{2:0.00}分", strName, chinese + math + english, (chinese + math + english) / 3);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
