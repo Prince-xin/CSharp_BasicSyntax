77900f6c70e324e0253f8062ceb7db06fe084416
Prince-xin <2300799210@qq.com>
2021-06-16 23:25:24 +0800

    三元表达式与随机数

---
+++ CSharpBasicSyntax/learn14_sanyuanBDS/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace learn14_sanyuanBDS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 三元表达式
+             *  语法：
+             *      表达式 ? 值1 : 值2;
+             *  释义：表达式：结果只能为bool类型（True/False）
+             *       值1 与 值2 的类型要一致，且与最后被赋值的变量表达式一致
+             *     当表达式为True时，结果返回 值1；
+             *     当表达式为False时，结果返回 值2；
+             */
+
+            int num01 = 100;
+            int num02 = 200;
+            int max = num01 > num02 ? num01 : num02;
+            Console.WriteLine(max);
+
+
+            /*
+             * Random
+             * 产生随机数
+             *      创建对象：Random 变量名 = new Random();
+             *      调用方法：int number = 变量名.Next(1,11); //随机数产生区间1~10，左闭右开
+             */
+            Random r = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                int number = r.Next(1, 10);
+                Console.WriteLine(number);
+            }
+            
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
