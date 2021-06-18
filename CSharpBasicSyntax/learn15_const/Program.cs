4ab5a7c34c025d1ca9d312d7ca1e6bc977b27994
Prince-xin <2300799210@qq.com>
2021-06-18 22:28:58 +0800

    常量(const)的初识

---
+++ CSharpBasicSyntax/learn15_const/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace learn15_const
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 常量(const)
+             *  释义：只可以赋值一次，复制后不可以修改值
+             *  语法：const 数据类型 变量名 = 值;
+             */
+            const int number = 10;
+            
+        }
+    }
+}
